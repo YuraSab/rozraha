c5a5729bf37e0463485d4ac8683258d1b845f1b8
Yura Sabat <yrasabat@gmail.com>
2022-11-19 20:20:51 +0200

    v1(based structure works)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import emailjs from '@emailjs/browser';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+
+    emailjs.init("zjyl1w2MBoCE6shQX");
+
+
+
+    function sendMail() {
+        let params = {
+            name: document.getElementById("name").value ,
+            email: document.getElementById("email").value ,
+            message: document.getElementById("message").value ,
+        };
+
+        const serviceID = "service_vhny0vn";
+        const templateID = "template_isbblxd";
+
+
+        emailjs.send(serviceID, templateID, params)
+            .   then(
+                res => {
+                    document.getElementById("name").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("message").value = "";
+                    alert("your message successfully");
+                    console.log(res);
+                }
+            )
+            .catch((err) => console.log(err));
+    }
+
+    return (
+        <div>
+            <input type="text" placeholder="Enter your name" id="name"/>
+            <input type="text" placeholder="Enter your email" id="email"/>
+            <textarea id="message" rows="3"></textarea>
+            <button onClick={sendMail}>Submit</button>
+        </div>
+    );
+};
 
 export default App;
